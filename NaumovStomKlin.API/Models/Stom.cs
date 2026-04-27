d30610ea09823684bc0c4f2bff45347a50e4e862
catdog <228p@BTK.MAIN>
2026-04-27 13:33:20 +0300

    создал класс

---
+++ NaumovStomKlin.API/Models/Stom.cs
@@ -0,0 +1,11 @@
+﻿namespace NaumovStomKlin.API.Models
+{
+    public class Stom
+    {
+        public int Id { get; set; }
+        public string medicine { get; set; }
+        public string specific_medicine { get; set; }
+        public int expiration_date { get; set; }
+        public decimal Price { get; set; }
+    }
+}
